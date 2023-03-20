a7157a692d1c18ec0476db5fcbea5eb25d784636
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2023-03-20 13:57:56 +0700

    Retrieve content from price file

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
+	// Price array
+	int pArr[100][2];
+	int bArr[2][200];
+
 
 	// Open the 2 file
 	ifstream pfile("price.txt");
@@ -10,5 +15,31 @@ int main() {
 
 	if (pfile.is_open() && bfile.is_open())
 		cout << "YES" << endl;
+
+	// Get the total line in each file
+	int n1, n2;
+	string line;
+
+
+	pfile >> n1;
+
+	
+	// Move the content from the price.txt file to the array
+	for (int i = 0; i < n1; i++)
+	{
+		pfile >> pArr[i][0];
+		cout << pArr[i][0] << " ";
+		pfile >> pArr[i][1];
+		cout << pArr[i][1] << endl;
+	}
+	
+
+
+	bfile >> n2;
+	// Move the next  line in the file, the line will be emtpy
+	getline(bfile, line);
+
+
+	
 	return 0;
 }
