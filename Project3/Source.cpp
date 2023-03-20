8595db8bd790c476017181521b3eeaa937b42cb7
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2023-03-20 16:38:56 +0700

    Get n1,n2

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -12,6 +12,16 @@ int main() {
 
 	if (libFile.is_open() && authFile.is_open())
 		cout << "YES" << endl;
+
+	// Get n1 and n2;
+	string line;
+	int n1, n2;
+	libFile >> n1;
+	authFile >> n2;
+
+	// Move the next line
+	getline(libFile,line);
+	getline(authFile, line);
 	return 0;
 }
 
