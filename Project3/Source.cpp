2ac9e490350cadd1f68bd224a7d540383cd90112
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2023-03-20 16:36:42 +0700

    Opened successfully

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 int main() {
 	
+	// Open the 2 files
+	ifstream libFile("library.txt");
+	ifstream authFile("author.txt");
+
+	if (libFile.is_open() && authFile.is_open())
+		cout << "YES" << endl;
 	return 0;
 }
 
